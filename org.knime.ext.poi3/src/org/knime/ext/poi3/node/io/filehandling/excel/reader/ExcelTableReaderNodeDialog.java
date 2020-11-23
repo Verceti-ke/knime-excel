ad4fd6f3aac14f8d76f4c1e6add12927e94a715a
Lars Schweikardt <lars.schweikardt@knime.com>
2020-11-23 12:18:59 +0100

    AP-15105: Upd. dialogs which are using FileChooser
    Updated all dialog which make use of an
    AbstractDialogComponentFileChooser and added the onClose call to the the
    onClose method of the dialog.
    
    AP-15015 (File chooser swing workers aren't cancelled when the dialog is
    closed without saving)

--- org.knime.ext.poi3/src/org/knime/ext/poi3/node/io/filehandling/excel/reader/ExcelTableReaderNodeDialog.java
+++ org.knime.ext.poi3/src/org/knime/ext/poi3/node/io/filehandling/excel/reader/ExcelTableReaderNodeDialog.java
@@ -921,5 +921,6 @@ final class ExcelTableReaderNodeDialog extends AbstractTableReaderNodeDialog<Exc
     public void onClose() {
         super.onClose();
         m_fileContentPreviewController.onClose();
+        m_filePanel.onClose();
     }
 }
